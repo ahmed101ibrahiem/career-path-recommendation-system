7e4a5d7430259a9fbb6af732cff081e283c6d598
AhmedElsab3 <Ahmed.elsab315@gmail.com>
2022-02-26 16:44:09 +0200

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:pharus/ui/screens/splash_screen.dart';
+import 'ui/screens/job_zone_screen.dart';
 import 'ui/screens/login.dart';
 import 'ui/screens/onboard.dart';
+import 'ui/screens/questionnaire.dart';
 import 'ui/screens/register.dart';
 import 'ui/screens/success.dart';
-
 void main() {
   runApp( MyApp());
 }
@@ -19,13 +20,15 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.teal,
         ),
-        home: SplashScreens(),
+        home: Questionnaire(),
         routes: {
           Login.id: (context) => Login(),
           Success.id: (context) => Success(),
           Register.id: (context) => Register(),
           OnboardingScreen.id: (context)=> OnboardingScreen(),
           SplashScreens.id: (context) => SplashScreens(),
+          Questionnaire.id: (context) => Questionnaire(),
+          JobZoneScreen.id: (context) => JobZoneScreen(),
 
         });
   }
